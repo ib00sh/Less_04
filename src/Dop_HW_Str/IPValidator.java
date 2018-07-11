a2e75d2a9333a6b5908595740db4c5e2b4b82fa9
ib00sh <wowmaxwow98@gmail.com>
2018-07-11 13:11:55 +0300

    Доп задание по паттернам

---
+++ src/Dop_HW_Str/IPValidator.java
@@ -0,0 +1,36 @@
+package Dop_HW_Str;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IPValidator {
+    private Pattern pattern;
+    private Matcher matcher;
+
+    public static void main(String[] args) {
+        IPValidator ipValidator = new IPValidator();
+
+            System.out.println("Результат: " + ipValidator.validate("192.168.0.0"));
+            System.out.println("Результат: " + ipValidator.validate("192.168.0"));
+            System.out.println("Результат: " + ipValidator.validate("192"));
+            System.out.println("Результат: " + ipValidator.validate("225.225.225.0"));
+            System.out.println("Результат: " + ipValidator.validate("8.8.8.8"));
+
+            }
+
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+    public IPValidator(){
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+    }
+
+    public boolean validate(final String ip){
+        matcher = pattern.matcher(ip);
+        return matcher.matches();
+    }
+
+}
