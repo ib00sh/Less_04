dd51dd2eecc17f3f2e4169290de5a4c7df058a1f
ib00sh <wowmaxwow98@gmail.com>
2018-07-16 12:38:15 +0300

    убрал строки для проверки

--- src/Less_05/Main.java
+++ src/Less_05/Main.java
@@ -3,9 +3,9 @@ package Less_05;
 public class Main {
     public static void main(String[] args) {
         Array array = new Array();
-        System.out.println("Начал выполняться первый метод");
+       
         array.create_arr();
-        System.out.println("Начал выполняться второй метод");
+
         array.part_arr();
     }
 }
